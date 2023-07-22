f5a6790c85be427bd028d345728dccfbc76e5dfb
Jessie Shockey <shockeyjessie92@gmail.com>
2023-07-21 19:36:16 -0600

    Added Finished page, updated eventlistener, score and intial in local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,6 +8,22 @@ var pEl = document.createElement("p");
 var listEl = document.createElement("ul");
 var liEl = document.createElement("li");
 var answerText = document.createElement("div");
+
+//FORM
+var form = document.createElement("form");
+var label = document.createElement("label");
+var input = document.createElement("input");
+var submit = document.createElement("input");
+form.setAttribute("id", "capture-scores");
+form.setAttribute("name", "capture-scores");
+form.setAttribute("onsubmit", "recordInitial()");
+label.setAttribute("for", "initials");
+input.setAttribute("type", "text");
+input.setAttribute("id", "initials");
+input.setAttribute("name", "initials");
+submit.setAttribute("type", "submit");
+submit.setAttribute("value", "Submit");
+
 //Static elements within HTML
 body.children[0].appendChild(h1El);
 body.children[0].appendChild(timer);
@@ -17,12 +33,16 @@ main.appendChild(h2El);
 main.appendChild(pEl);
 main.appendChild(liEl);
 main.appendChild(listEl);
+// sets classes for the start and answer buttons for use in event listeners
+liEl.setAttribute ("class", "start");
 listEl.setAttribute ("class", "answers");
+var startbttn = document.querySelector(".start");
+var answerbttn = document.querySelector(".answers");
 
 // Global variables for score, questions index, and timer
 var score = 0;
 var questionIndex =  0;
-var timeLeft = 5;
+var timeLeft = 20;
 
 //Landing Page Setup BEGIN
 var welcomeScreen = {
@@ -30,9 +50,11 @@ var welcomeScreen = {
     info: "Try to answer the following code-related questions within the time limit. Keep in mind that incorrect answers will penalize your score/time by ten seconds!",
     button: "Start Quiz"
 }
-h2El.textContent = welcomeScreen.header;
-pEl.textContent = welcomeScreen.info;
-liEl.textContent = welcomeScreen.button;
+function init(){
+    h2El.textContent = welcomeScreen.header;
+    pEl.textContent = welcomeScreen.info;
+    liEl.textContent = welcomeScreen.button;
+}
 
 // Question & Answers object array
 var questions = [
@@ -73,7 +95,6 @@ function loadOptions(){
     var index = 0;
     questions[questionIndex].options.forEach(element => {
         element = document.createElement("li");
-        
         listEl.appendChild(element);
         element.textContent = questions[questionIndex].options[index];
         index++;
@@ -94,36 +115,62 @@ function loadQuestion(){
     if (questionIndex<questions.length){
         h2El.textContent = questions[questionIndex].question;
         loadOptions();
+    }else{
+        loadFinished ();
     }
 };   
 
-// listens for correct answer to be selected
-addEventListener("click", function(event){
-    var answer = event.target;
-    main.appendChild(answerText);
-        if (answer.textContent===questions[questionIndex].correct){
-            score++;
-            questionIndex++;
-            answerText.textContent = "Correct!";     
-            this.setTimeout(removeOptions,300);
-            this.setTimeout(loadQuestion,300);
-        }else if (answer.textContent==="Start Quiz"){
+// loads the final page after quiz is completed
+function loadFinished (){
+    score = timeLeft;
+    h2El.textContent = "All Done!";
+    main.appendChild(pEl);
+    pEl.textContent = "Your final score is "+ score +"."
+    main.appendChild(form);
+    main.children[3].appendChild(label);
+    main.children[3].appendChild(input);
+    main.children[3].appendChild(submit);
+
+    label.textContent = "Enter initials";
+}
+
+function recordInitial(){
+    var initial = document.forms["capture-scores"]["initials"].value;
+    var finalScore= {
+        userInitials: initial,
+        finScore: score
+    };
+        localStorage.setItem("finalScore",JSON.stringify(finalScore));
+
+    
+}
+// EventListener for "Start Quiz" button
+startbttn.addEventListener("click", function(){
             main.removeChild(pEl);
             main.removeChild(liEl);
             setTime();
             loadQuestion();
+});
+
+// EventListener for Answer options
+answerbttn.addEventListener("click", function(event){
+    var answer = event.target;
+    main.appendChild(answerText);
+        if (answer.textContent===questions[questionIndex].correct){
+            questionIndex++;
+            answerText.textContent = "Correct!";     
+            setTimeout(removeOptions,300);
+            setTimeout(loadQuestion,300);
         }else {
             answerText.textContent = "Incorrect!";
             questionIndex++;
             timeLeft=timeLeft-10;
-            this.setTimeout(removeOptions,300);
-            this.setTimeout(loadQuestion,300);
+            setTimeout(removeOptions,300);
+            setTimeout(loadQuestion,300);
         }
-    
 });
 
-
-
+init();
 
 
 
