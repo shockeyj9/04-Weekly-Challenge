82a5f33989872d8a64af74ff21822cb5da852a05
Jessie Shockey <shockeyjessie92@gmail.com>
2023-07-24 22:12:42 -0600

    add highscores.js comments and styling to highscores page

--- assets/js/highscores.js
+++ assets/js/highscores.js
@@ -1,30 +1,38 @@
+// global variables
 var goBack = document.getElementById("back");
 var clearHighscore = document.getElementById("clear-highscores");
 
+// takes the scores out of memory and creates an ordered list for each score. Goes from highest score to lowest score
 function renderFinalScore(){
     var storedScores = JSON.parse(localStorage.getItem("finalScore"));
-    var storedScores = storedScores.sort(
-      (p1,p2) => (p1.score < p2.score) ? 1 : (p1.score>p2.score) ? -1 : 0
+    //Sorting functionality occurs here
+    var storedScores = storedScores.sort( 
+      (s1,s2) => (s1.score < s2.score) ? 1 : (s1.score>s2.score) ? -1 : 0
     );
 
         for (var i = 0; i < storedScores.length; i++) {
             var highScores = storedScores[i];
-            var ulEL = document.querySelector('ul');
+            var ulEL = document.querySelector('ol');
             var li = document.createElement("li");
             li.setAttribute("id", "highscore")
             ulEL.appendChild(li);
             li.textContent = highScores.initial.toUpperCase() + " " + highScores.score;
-          }
         }
+};
 
+// When page loads it runs renderFinalScore which prints all the scores 
 addEventListener("load", renderFinalScore );
+
+// Back button takes you Quiz
 goBack.addEventListener("click", function(event){
   history.back();
 });
+
+// Clears history
 clearHighscore.addEventListener("click",function(event){
   localStorage.clear();
   var list = document.querySelectorAll("li");
   list.forEach(lis =>{
     lis.remove();
-});
+  });
 });
